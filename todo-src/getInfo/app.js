f222c86f14d690bde0720fcbcdc6933864fef2dc
Michael Karpov <mvladk@gmail.com>
2021-03-30 18:06:29 +0300

    getInfo tmplate

--- todo-src/getInfo/app.js
+++ todo-src/getInfo/app.js
@@ -4,14 +4,13 @@
 // default imports
 const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
-const { metricScope, Unit } = require("aws-embedded-metrics")
-const DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" })
-
+const {metricScope, Unit} = require("aws-embedded-metrics")
+const DDB = new AWS.DynamoDB({apiVersion: "2012-10-08"})
 
 // environment variables
-const { TABLE_NAME, ENDPOINT_OVERRIDE, REGION } = process.env
-const options = { region: REGION }
-AWS.config.update({ region: REGION })
+const {TABLE_NAME, ENDPOINT_OVERRIDE, REGION} = process.env
+const options = {region: REGION}
+AWS.config.update({region: REGION})
 
 if (ENDPOINT_OVERRIDE !== "") {
     options.endpoint = ENDPOINT_OVERRIDE
@@ -22,16 +21,10 @@ const docClient = new AWS.DynamoDB.DocumentClient(options)
 const response = (statusCode, body, additionalHeaders) => ({
     statusCode,
     body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', ...additionalHeaders },
+    headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', ...additionalHeaders},
 })
 
-function isValidRequest(context, event) {
-    return (event !== null) &&
-        (event.pathParameters !== null) &&
-        (event.pathParameters.id !== null) &&
-        (/^[\w-]+$/.test(event.pathParameters.id))
-}
-
+// Get cognito username from claims
 function getCognitoUsername(event){
     let authHeader = event.requestContext.authorizer;
     if (authHeader !== null)
@@ -42,39 +35,40 @@ function getCognitoUsername(event){
 
 }
 
-function getRecordById(username, recordId) {
+// Retrieve all the items by cognito-username
+function getRecords(username) {
     let params = {
         TableName: TABLE_NAME,
-        Key: {
-            "cognito-username": username,
-            "id": recordId
+        KeyConditionExpression: "#username = :username",
+        ExpressionAttributeNames:{
+            "#username": "cognito-username"
+        },
+        ExpressionAttributeValues: {
+            ":username": username
         }
     }
 
-    return docClient.get(params)
+    return docClient.query(params)
 }
 
 // Lambda Handler
-exports.getInfo =
+exports.getInfoFunc =
     metricScope(metrics =>
         async (event, context, callback) => {
             metrics.setNamespace('TodoApp')
-            metrics.putDimensions({ Service: "getTodo" })
+            metrics.putDimensions({Service: "getInfo"})
             metrics.setProperty("RequestId", context.requestId)
-            if (!isValidRequest(context, event)) {
-                metrics.putMetric("Error", 1, Unit.Count)
-                return response(400, { message: "Error: Invalid request" })
-            }
 
             try {
                 let username = getCognitoUsername(event);
-                let data = await getRecordById(username, event.pathParameters.id).promise()
+                let data = await getRecords(username).promise()
                 metrics.putMetric("Success", 1, Unit.Count)
                 return response(200, data)
 
             } catch (err) {
                 metrics.putMetric("Error", 1, Unit.Count)
-                return response(400, { message: err.message })
+                console.error(err.message);
+                return response(400, {message: err.message})
             }
         }
     )
