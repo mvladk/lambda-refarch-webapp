0b9189639b8f4716e68f7e66127d55e26778202f
Michael Karpov <mvladk@gmail.com>
2021-04-06 16:35:14 +0300

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "79qervupji6a6484gvhe5vtv2n",     // CognitoClientID
-  "api_base_url": "https://w2fnutild0.execute-api.eu-west-1.amazonaws.com/staging",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "4fbgbmnhkg5lhl0bvijd5n1u8b",     // CognitoClientID
+  "api_base_url": "https://1akunnlii3.execute-api.eu-west-1.amazonaws.com/staging",                                     // TodoFunctionApi
   "cognito_hosted_domain": "exchange-exchange-lambda-lib.auth.eu-west-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1o63bejvyf9mw.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d1wjiz0vgchd5s.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
